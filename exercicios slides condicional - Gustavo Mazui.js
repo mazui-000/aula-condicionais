1075e2e3e58e60d34c5e63d88912e6c1000c0356
Gustavo Mazui <gustavopmazui18@gmail.com>
2024-09-30 11:15:36 -0300

    Add files via upload

---
+++ exercicios slides condicional - Gustavo Mazui.js
@@ -0,0 +1,92 @@
+///////////////////////exercicio slide 1 
+
+/*let a = Number(prompt("Digite um numero:"))
+let b = Number(prompt("Digite outro numero:"))
+
+function comparar(num1 , num2) {
+   if (num1 === num2) {
+
+   
+console.log("Sucesso!")
+   }
+}
+
+comparar (a, b)*/
+
+
+
+
+///////////////////////exercicio slide 2 
+
+/* let a = Number(prompt("Digite um numero:"))
+let b = Number(prompt("Digite outro numero:"))
+
+function comparar(num1 , num2) {
+   if (num1 === num2) {
+
+   
+console.log(" são iguais!")
+
+   } else {
+      console.log("não são iguais")
+   }    
+}
+
+comparar (a, b)*/
+
+
+///////////////////////exercicio slide 3
+/*
+let a = Number(prompt("Digite um numero:"))
+let b = Number(prompt("Digite outro numero:"))
+
+function comparar(num1 , num2) {
+   if (num1 === num2) {
+
+   
+console.log(" são iguais!")
+
+   } else if(num1 > num2) {
+      console.log("Primeiro numero é maior que o segundo numero")
+   }    else {
+      console.log("Primeiro numero é menor que o segundo numero")
+   }
+}
+
+comparar (a, b) */ 
+
+
+///////////////////////exercicio slide 4 
+/*let pokemom = prompt ("Escolha um Pokemon")
+
+switch (pokemom) {
+   case 'bulbasauro':
+      console.log ('planta e veneno')
+      break
+   case 'charmander':
+      console.log ('fogo')
+       break
+   case 'squirtle':
+   console.log ('água')
+   break
+   default:
+      console.log ("esse pokemon não existe, tente dnv")
+}
+*/  
+
+
+///////////////////////exercicio slide 5 
+
+/*let facul = prompt("voce já está cursando uma faculdade ?")
+let ensinoMedio= prompt("Você concluiu o Ensino Medio ?")
+let idade= Number(prompt("Qual é a sua idade?"))
+
+   function infos(facul , ensinoMedio, idade) {
+      if (facul === "não" && ensinoMedio === "sim" && idade>=18) {
+         console.log ("Você pode cursar faculdade!")
+      }else{
+         console.log ("Você não pode cursar faculdade!")
+      }
+      }     
+
+   infos(facul, ensinoMedio, idade)*/
