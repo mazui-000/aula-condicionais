0aa5079445b7895aee9d297f1d37a0952bdac8cf
Gustavo Mazui <gustavopmazui18@gmail.com>
2024-10-01 11:05:44 -0300

    Add files via upload

---
+++ Exercicios Condicionais - Gustavo Mazui.js
@@ -0,0 +1,102 @@
+////////////Exercicio de Interpretação 1
+
+//a) O codigo verifica se o numero digitado é divisivel por 2. 
+
+//b) imprime "passou no teste" quando o numero digitado é divisivel por 2. 
+
+//c) imprime "não passou no teste" quando o numero digitado não é divisivel por 2.
+
+
+
+
+////////////Exercicio de Interpretação 2
+
+//a) lista frutas e seus valores.
+
+//b) será impresso o preço da maça escolhida. 
+
+//c) o preço da pêra passa a ser 5, pelo valor do default abaixo
+
+
+
+
+
+////////////Exercicio de Interpretação 3
+
+//a) Guardando na prompt o numero que o usuario escolher. 
+
+//b) Será impresso "Esse número passou no teste", pelo 10 ser maior que o 0. Já se for escrito -10 dará erro
+
+//c) O erro do codigo é ele estar dentro do escopo local, nao podendo ser acessado de fora.
+
+
+
+
+
+////////////Exercicio de Escrita de Codigo 1
+
+/* let idade = Number (prompt("Qual é sua idade?"))
+if (idade >18){
+ console.log("Você pode dirigir")
+
+}else{ 
+console.log("Você não pode dirigir")
+
+} */
+
+
+
+
+////////////Exercicio de Escrita de Codigo 2
+
+/* let turno = prompt ("Digite M, V, N, referente ao turno que você estuda. (Maturno, Vespertino ou Noturno")
+
+if (turno === "M") {
+    console.log("Bom Dia!")
+}else if(turno === "V"){ 
+    console.log("Boa Tarde!")
+}else if (turno === "N"){
+    console.log("Boa Noite!")
+} */ 
+
+
+
+
+
+////////////Exercicio de Escrita de Codigo 3
+
+/* let turno = prompt ("Digite M, V, N, referente ao turno que você estuda. (Maturno, Vespertino ou Noturno") 
+
+switch (turno){
+case "M":
+console.log ("Bom dia!")
+break 
+
+case "V": 
+console.log ("Boa Tarde")
+break
+
+case "N":
+console.log("Boa Noite")
+break
+default: 
+console.log ("ohhh magaaa")    
+} */ 
+ 
+
+
+
+
+////////////Exercicio de Escrita de Codigo 4
+
+ 
+ let filme = prompt ("Qual filme voce quer assistir ?") 
+
+let ingresso = prompt ("Quantos mangos quer pagar no ingresso")
+
+if (filme === "fantasia" &&  ingresso <15) {
+    console.log ("Bom filme") 
+} else { 
+console.log ("Escolha outro filme :(") }   
+    
+    
